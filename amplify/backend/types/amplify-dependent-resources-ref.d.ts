9790cae8e400f24d1b6eedf5548599108f003445
Empereur <bartosikpatrick2@gmail.com>
2023-12-20 09:31:15 +0100

    :memo: add auth and update api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,25 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
+  },
+  "auth": {
+    "devopstodobf2fe34f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "graphQlLambdaAuthorizer77d1f53a": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-// import { Auth } from 'aws-amplify';
 import LoginForm from '@/components/LoginForm';
-import RegisterForm from '@/app/register/page';
-import { App } from '@/components/withAuthenticator';
 
 export default function Home() {
-
     return (
         <div className="flex justify-center items-center h-screen bg-gray-100">
             <LoginForm />
