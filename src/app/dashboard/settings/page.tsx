66885ed53c2e333a527342f53c3dbd7057e31db5
Empereur <bartosikpatrick2@gmail.com>
2023-12-21 11:55:05 +0100

    🐛 corriger(page.tsx) : importer la fonction updatePassword et le type UpdatePasswordInput depuis aws-amplify/auth ✨ fonctionnalité(page.tsx) : ajouter la fonctionnalité de changement de mot de passe La fonction updatePassword et le type UpdatePasswordInput sont importés depuis aws-amplify/auth pour permettre la mise à jour du mot de passe de l'utilisateur. Une interface Passwords est également ajoutée pour gérer les champs de mot de passe dans le formulaire. La fonction handlePasswordChange est ajoutée pour gérer le changement de mot de passe en vérifiant que les nouveaux mots de passe correspondent et en appelant la fonction updatePassword.

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -3,6 +3,7 @@
 import NavBar from '@/components/dashboard/NavBar';
 import { useEffect, useState } from 'react';
 import { fetchUserAttributes } from 'aws-amplify/auth';
+import { updatePassword, type UpdatePasswordInput } from 'aws-amplify/auth';
 import { Title, Text, Card, Divider, TextInput, Button } from '@tremor/react';
 import { ToastContainer, toast } from 'react-toastify';
 
@@ -18,8 +19,24 @@ interface UserAttributes {
     email: string;
 }
 
+interface Passwords {
+    oldPassword: string;
+    newPassword: string;
+    confirmNewPassword: string;
+}
+
 const UserSettingsPage = () => {
-    const [user, setUser] = useState<UserAttributes>({ firstName: '', lastName: '', email: '' });
+    const [user, setUser] = useState<UserAttributes>({
+        firstName: '',
+        lastName: '',
+        email: ''
+    });
+    const [passwords, setPasswords] = useState<Passwords>({
+        oldPassword: '',
+        newPassword: '',
+        confirmNewPassword: ''
+    });
+
 
     async function fetchCurrentUser() {
         try {
@@ -39,13 +56,15 @@ const UserSettingsPage = () => {
 
     async function handleUpdateEmailAndNameAttributes(
         updatedName: string,
-        updateFamilyName: string
+        updateFamilyName: string,
+        updateEmail: string
     ) {
         try {
             const attributes = await updateUserAttributes({
                 userAttributes: {
                     name: updatedName,
-                    family_name: updateFamilyName
+                    family_name: updateFamilyName,
+                    email: updateEmail
                 }
             });
             toast.success("Profil mis à jour avec succès !");
@@ -54,6 +73,24 @@ const UserSettingsPage = () => {
             console.log(error);
         }
     }
+
+    const handlePasswordChange = async () => {
+        if (passwords.newPassword !== passwords.confirmNewPassword) {
+            toast.error("Les nouveaux mots de passe ne correspondent pas.");
+            return;
+        }
+        try {
+            await updatePassword({
+                oldPassword: passwords.oldPassword,
+                newPassword: passwords.newPassword
+            });
+            toast.success("Mot de passe mis à jour avec succès !");
+            setPasswords({ oldPassword: '', newPassword: '', confirmNewPassword: '' });
+        } catch (error) {
+            console.error('Erreur lors de la mise à jour du mot de passe:', error);
+            toast.error("Erreur lors de la mise à jour du mot de passe.");
+        }
+    };
 
     useEffect(() => {
         fetchCurrentUser();
@@ -91,7 +128,7 @@ const UserSettingsPage = () => {
                                 <TextInput
                                     placeholder={user.email}
                                     type='email'
-                                    disabled
+                                    onChange={(e) => setUser({ ...user, email: e.target.value })}
                                 />
                             </div>
                             <div>
@@ -104,8 +141,33 @@ const UserSettingsPage = () => {
                             </div>
                         </div>
                         <div>
-                            <Button size='md' onClick={() => handleUpdateEmailAndNameAttributes(user.firstName, user.lastName)}>Sauvegarder les Modification</Button>
+                            <Button size='md' onClick={() => handleUpdateEmailAndNameAttributes(user.firstName, user.lastName, user.email)}>Sauvegarder les Modification</Button>
                         </div>
+                    </div>
+                </Card>
+
+                <Card className="mt-6 flex flex-col gap-7 items-end">
+                    <Divider>Changer le mot de passe</Divider>
+                    <div className='w-full flex flex-col gap-6 items-end'>
+                        <TextInput
+                            placeholder="Ancien mot de passe"
+                            type="password"
+                            value={passwords.oldPassword}
+                            onChange={(e) => setPasswords({ ...passwords, oldPassword: e.target.value })}
+                        />
+                        <TextInput
+                            placeholder="Nouveau mot de passe"
+                            type="password"
+                            value={passwords.newPassword}
+                            onChange={(e) => setPasswords({ ...passwords, newPassword: e.target.value })}
+                        />
+                        <TextInput
+                            placeholder="Confirmer le nouveau mot de passe"
+                            type="password"
+                            value={passwords.confirmNewPassword}
+                            onChange={(e) => setPasswords({ ...passwords, confirmNewPassword: e.target.value })}
+                        />
+                        <Button size='md' onClick={handlePasswordChange}>Changer le mot de passe</Button>
                     </div>
                 </Card>
             </div>
